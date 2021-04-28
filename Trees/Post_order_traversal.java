acb6f2e4646968ee2f829ed22a87d1cf8a278376
Mayraju <53441673+Mayraju@users.noreply.github.com>
2021-04-28 10:55:17 +0530

    Create Post_order_traversal.java

---
+++ Trees/Post_order_traversal.java
@@ -0,0 +1,26 @@
+/*
+Complete the function postOrderTraversal given below.
+For your reference:
+
+class Node
+{
+    long value;
+    Node left, right;
+    public Node(long item)
+    {
+        value = item;
+        left = right = null;
+    }
+}
+*/
+
+void postOrderTraversal(Node node) {
+    //write your code here
+    if(node == null)
+    {
+      return;
+    }
+    postOrderTraversal(node.left);
+    postOrderTraversal(node.right);
+    System.out.print(node.value+" ");
+}
