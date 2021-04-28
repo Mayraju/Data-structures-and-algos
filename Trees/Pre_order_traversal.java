62962d5a23a7e3ffd2cbaaff74fc711f6753cb6a
Mayraju <53441673+Mayraju@users.noreply.github.com>
2021-04-28 10:55:49 +0530

    Create Pre_order_traversal.java

---
+++ Trees/Pre_order_traversal.java
@@ -0,0 +1,26 @@
+/* Complete the function preOrderTraversal given below
+For your reference
+class Node
+{
+    long value;
+    Node left, right;
+    public Node(long item)
+    {
+        value = item;
+        left = right = null;
+    }
+}
+*/
+
+void preOrderTraversal(Node node) {
+    
+    //write your code here
+    if(node == null)
+    {
+      return ;
+    }
+    System.out.print(node.value+" ");
+    preOrderTraversal(node.left);
+    preOrderTraversal(node.right);
+
+}
