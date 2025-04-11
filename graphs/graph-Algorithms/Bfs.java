a119c74b7727d257bc8ef414d5e577e70c2700a7
Mayraju <mayraju777@gmail.com>
2025-04-11 13:21:00 +0530

    bugs fixed

--- graphs/graph-Algorithms/Bfs.java
+++ graphs/graph-Algorithms/Bfs.java
@@ -3,48 +3,48 @@
 // BFS traverse level by level
 // BFS uses the queue data structre.
 // the graph we are using in this code as undirected graph.
+import java.util.*;
 
-
-class Bfs{
-    public static void main(String[] args){
+class Bfs {
+    public static ArrayList<Integer> main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int V = sc.nextInt() // no of vertices V=vertices
-        int e = sc.nextInt() // no of edges e = edges
-        int[][] edges = new int[e][2];  // how veritices are connected each other;
-        for(int i=0;i<e;i++){
+        int V = sc.nextInt(); // no of vertices V=vertices
+        int e = sc.nextInt(); // no of edges e = edges
+        int[][] edges = new int[e][2]; // how veritices are connected each other;
+        for (int i = 0; i < e; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
-            edges[i][0] =u;
-            edges[i][1] =v;
+            edges[i][0] = u;
+            edges[i][1] = v;
         }
         // how to form the graph from the edges
         ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
         // This for loop makes graph like this =[[],[],[],[]]
-        for(int i=0;i<V;i++){
+        for (int i = 0; i < V; i++) {
             graph.add(new ArrayList<Integer>());
         }
         // we are making the undirected graph
-        for(int i=0;i<edges.length;i++){
-             int u = edges[i][0];
-             int v = edges[i][1];
-             graph.get(u).add(v);
-             graph.get(v).add(u);
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
 
-        boolean[] visited = new boolean[V+1];
+        boolean[] visited = new boolean[V + 1];
         ArrayList<Integer> bfs = new ArrayList<Integer>();
-        Arrays.fill(visited,false);
+        Arrays.fill(visited, false);
         Queue<Integer> queue = new LinkedList<Integer>();
-        queue.add(0) //start with some vertext or any vertex
+        queue.add(0); // start with some vertext or any vertex
         visited[0] = true;
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int curr = queue.poll();
             bfs.add(curr);
             ArrayList<Integer> nghrs = graph.get(curr);
-            for(int ele:nghrs){
-                if(visited[ele] == false){
-                     visited[ele]=true;
-                     queue.add(ele);
+            for (int ele : nghrs) {
+                if (visited[ele] == false) {
+                    visited[ele] = true;
+                    queue.add(ele);
                 }
             }
         }
--- graphs/graph-Algorithms/Dfs.java
+++ graphs/graph-Algorithms/Dfs.java
@@ -4,51 +4,58 @@
 // DFS uses the recursion
 // In this code we are using the undirected graph
 // used stack space for recursion
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.PriorityQueue;
+import java.util.Stack;
 
-class Dfs{
-    public static void main(String[] args){
+class Dfs {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int V = sc.nextInt() // no of vertices V=vertices
-        int e = sc.nextInt() // no of edges e = edges
-        int[][] edges = new int[e][2];  // how veritices are connected each other;
-        for(int i=0;i<e;i++){
+        int V = sc.nextInt(); // no of vertices V=vertices
+        int e = sc.nextInt(); // no of edges e = edges
+        int[][] edges = new int[e][2]; // how veritices are connected each other;
+        for (int i = 0; i < e; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
-            edges[i][0] =u;
-            edges[i][1] =v;
+            edges[i][0] = u;
+            edges[i][1] = v;
         }
         // how to form the graph from the edges
         ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
         // This for loop makes graph like this =[[],[],[],[]]
-        for(int i=0;i<V;i++){
+        for (int i = 0; i < V; i++) {
             graph.add(new ArrayList<Integer>());
         }
         // we are making the undirected graph
-        for(int i=0;i<edges.length;i++){
-             int u = edges[i][0];
-             int v = edges[i][1];
-             graph.get(u).add(v);
-             graph.get(v).add(u);
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
 
-        boolean[] visited = new boolean[V+1];
+        boolean[] visited = new boolean[V + 1];
         ArrayList<Integer> dfs = new ArrayList<Integer>();
-        Arrays.fill(visited,false);
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                dfsTraversal(i,graph,visited,dfs);
+        Arrays.fill(visited, false);
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfsTraversal(i, graph, visited, dfs);
             }
         }
     }
 
-    public void dfsTraversal(int vertex,ArrayList<ArrayList<Integer>> graph,int[] visited,ArrayList<Integer> dfs){
+    public static void dfsTraversal(int vertex, ArrayList<ArrayList<Integer>> graph, boolean[] visited,
+            ArrayList<Integer> dfs) {
         visited[vertex] = true;
         dfs.add(vertex);
         ArrayList<Integer> nghrs = graph.get(vertex);
-        for(int ele : nghrs){
-             if(visited[ele] == false){
-                 dfsTraversal(ele,graph,visited,dfs);
-             }
+        for (int ele : nghrs) {
+            if (visited[ele] == false) {
+                dfsTraversal(ele, graph, visited, dfs);
+            }
         }
     }
 }
